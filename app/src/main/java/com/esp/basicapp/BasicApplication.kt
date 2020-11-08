06244d80411d27369e981a1dd765f8d4196e12e8
Toshihiro Sekioka <toshihiro.sekioka@misoca.jp>
2020-11-08 22:52:17 +0900

    Timberを有効にする

--- app/src/main/java/com/esp/basicapp/BasicApplication.kt
+++ app/src/main/java/com/esp/basicapp/BasicApplication.kt
@@ -1,6 +1,16 @@
 package com.esp.basicapp
 
 import android.app.Application
+import timber.log.Timber
 
 class BasicApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Timberを有効にします
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
