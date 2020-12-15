28b77bc12c3b96861e30759541763c85c5f581ed
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2020-12-15 17:06:20 +0900

    指紋認証による暗号化処理を追加

--- app/src/main/java/com/esp/basicapp/MainActivity.kt
+++ app/src/main/java/com/esp/basicapp/MainActivity.kt
@@ -19,21 +19,25 @@ import java.security.KeyStore
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 import java.nio.charset.Charset
+import java.util.Enumeration
 import java.util.concurrent.Executor
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         private const val KEY_NAME = "bio_key"
     }
 
     private lateinit var executor: Executor
     private lateinit var biometricPrompt: BiometricPrompt
-    private lateinit var promptInfo: BiometricPrompt.PromptInfo
     private var canUseBiometrics = false
+    private var encryptedData: ByteArray? = null
+    private var secretKey: SecretKey? = null
+    private var iv: ByteArray? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,24 +48,31 @@ class MainActivity : AppCompatActivity() {
         // KTXを導入するとできる
         hello.text = "ハロー"
 
+        initializeCrypto()
         checkBiometric()
-        initializeBiometrics()
         btn_login.setOnClickListener {
-            if(canUseBiometrics){
-                // Exceptions are unhandled within this snippet.
+            if (canUseBiometrics) {
                 val cipher = getCipher()
                 val secretKey = getSecretKey()
-                cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+                if (encryptedData == null) {
+                    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+                    iv = cipher.iv
+                } else {
+                    val ivParameterSpec = IvParameterSpec(iv)
+                    cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec)
+                }
 
                 val promptInfo = BiometricPrompt.PromptInfo.Builder()
-                        .setTitle("Biometric login for my app")
-                        .setSubtitle("Log in using your biometric credential")
-                        .setNegativeButtonText("Use account password")
-                        .build()
+                    .setTitle("Biometric login for my app")
+                    .setSubtitle("Log in using your biometric credential")
+                    .setNegativeButtonText("Use account password")
+                    .build()
 
-                biometricPrompt.authenticate(promptInfo,
-                        BiometricPrompt.CryptoObject(cipher))
-            }else{
+                biometricPrompt.authenticate(
+                    promptInfo,
+                    BiometricPrompt.CryptoObject(cipher)
+                )
+            } else {
                 // Prompts the user to create credentials that your app accepts.
                 val enrollIntent = Intent(Settings.ACTION_SETTINGS).apply {
                 }
@@ -70,58 +81,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkBiometric(){
+    private fun checkBiometric() {
         val biometricManager = BiometricManager.from(this)
         when (biometricManager.canAuthenticate()) {
             BiometricManager.BIOMETRIC_SUCCESS -> {
                 Timber.d("App can authenticate using biometrics.")
                 canUseBiometrics = true
+                initializeBiometrics()
             }
             BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
                 Timber.e("No biometric features available on this device.")
             BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
-                Timber.d( "Biometric features are currently unavailable.")
+                Timber.d("Biometric features are currently unavailable.")
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
-                Timber.d( "NONE ENROLLED")
+                Timber.d("NONE ENROLLED")
             }
         }
     }
 
-    private fun initializeBiometrics(){
-        executor = ContextCompat.getMainExecutor(this)
-        biometricPrompt = BiometricPrompt(this, executor,
-                object : BiometricPrompt.AuthenticationCallback() {
-                    override fun onAuthenticationError(errorCode: Int,
-                                                       errString: CharSequence) {
-                        super.onAuthenticationError(errorCode, errString)
-                        Toast.makeText(applicationContext,
-                                "Authentication error: $errString", Toast.LENGTH_SHORT)
-                                .show()
-                    }
-
-                    override fun onAuthenticationSucceeded(
-                            result: BiometricPrompt.AuthenticationResult) {
-                        super.onAuthenticationSucceeded(result)
-                        Toast.makeText(applicationContext,
-                                "Authentication succeeded!", Toast.LENGTH_SHORT)
-                                .show()
-
-                        val encryptedInfo: ByteArray = result.cryptoObject.cipher?.doFinal(
-                                "hello".toByteArray(Charset.defaultCharset())
-                        )
-                    }
-
-                    override fun onAuthenticationFailed() {
-                        super.onAuthenticationFailed()
-                        Toast.makeText(applicationContext, "Authentication failed",
-                                Toast.LENGTH_SHORT)
-                                .show()
-                    }
-                })
-
-        generateSecretKey(KeyGenParameterSpec.Builder(
-                KEY_NAME,
-                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+    private fun initializeCrypto() {
+        generateSecretKey(
+            KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
                 .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
                 .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
                 .setUserAuthenticationRequired(true)
@@ -130,15 +110,63 @@ class MainActivity : AppCompatActivity() {
                 // on Android 7.0 (API level 24) or higher. The variable
                 // "invalidatedByBiometricEnrollment" is true by default.
                 //.setInvalidatedByBiometricEnrollment(true)
-                .build())
+                .build()
+        )
     }
 
+    private fun initializeBiometrics() {
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult
+                ) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT
+                    ).show()
+
+                    if (encryptedData == null) {
+                        val inputData = "hello".toByteArray(Charset.forName("utf-8"))
+                        val encryptedData = result.cryptoObject?.cipher?.doFinal(inputData)?.also {
+                            encryptedData = it
+                        }
+                        Timber.d(encryptedData.contentToString())
+                    } else {
+                        val decryptedData = result.cryptoObject?.cipher?.doFinal(encryptedData)
+                        Timber.d(decryptedData?.toString(Charset.forName("utf-8")))
+                    }
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            })
+    }
 
     private fun generateSecretKey(keyGenParameterSpec: KeyGenParameterSpec) {
         val keyGenerator = KeyGenerator.getInstance(
-                KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore"
+        )
         keyGenerator.init(keyGenParameterSpec)
-        keyGenerator.generateKey()
     }
 
     private fun getSecretKey(): SecretKey {
@@ -146,16 +174,14 @@ class MainActivity : AppCompatActivity() {
 
         // Before the keystore can be accessed, it must be loaded.
         keyStore.load(null)
-        return (keyStore.getKey(KEY_NAME, null) as SecretKey).also {
-            Timber.d(it.format)
-        }
+        return (keyStore.getKey(KEY_NAME, null) as SecretKey)
     }
 
     private fun getCipher(): Cipher {
-        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+        return Cipher.getInstance(
+            KeyProperties.KEY_ALGORITHM_AES + "/"
                 + KeyProperties.BLOCK_MODE_CBC + "/"
-                + KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                + KeyProperties.ENCRYPTION_PADDING_PKCS7
+        )
     }
-
-
 }
