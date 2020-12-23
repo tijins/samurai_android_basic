3e6a4ad3f03b4795a9e41eca23dac0dbc3f64e52
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2020-12-23 16:34:46 +0900

    DBへの保存・読み出しを追加

--- app/src/main/java/com/esp/basicapp/MainActivity.kt
+++ app/src/main/java/com/esp/basicapp/MainActivity.kt
@@ -1,28 +1,47 @@
 package com.esp.basicapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import com.esp.basicapp.data.User
+import com.esp.basicapp.data.UserDatabase
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.load
+import kotlinx.android.synthetic.main.activity_main.load_db
 import kotlinx.android.synthetic.main.activity_main.save
+import kotlinx.android.synthetic.main.activity_main.save_db
 import kotlinx.android.synthetic.main.activity_main.txt_age
 import kotlinx.android.synthetic.main.activity_main.txt_name
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.lang.Exception
 import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val FILE_NAME = "sample.json"
+        const val DB_FILE_NAME = "sample.db"
     }
+
+    // coroutine
+    private val job = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + job)
+
+    // Roomライブラリを使用する
+    private lateinit var db: UserDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Timber.d("onCreate")
+        // Roomライブラリを使用する
+        db = Room.databaseBuilder(this, UserDatabase::class.java, DB_FILE_NAME).build()
 
         save.setOnClickListener {
             saveSample()
@@ -32,13 +51,20 @@ class MainActivity : AppCompatActivity() {
             loadSample()
         }
 
+        save_db.setOnClickListener {
+            saveDatabase()
+        }
+
+        load_db.setOnClickListener {
+            loadDatabase()
+        }
     }
 
-    private fun saveSample(){
+    private fun saveSample() {
         val sample = Sample()
             .apply {
                 name = txt_name.text.toString()
-                age =  txt_age.text.toString().toInt()
+                age = txt_age.text.toString().toInt()
             }
 
         val gson = GsonBuilder().create()
@@ -54,7 +80,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadSample(){
+    private fun loadSample() {
         val gson = GsonBuilder().create()
 
         // アプリ用ディレクトリのファイルを開く
@@ -73,14 +99,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        if(sample != null){
+        if (sample != null) {
             txt_name.setText(sample.name)
             txt_age.setText(sample.age.toString())
         }
-
     }
 
-    private fun loadSampleWithError(){
+    private fun loadSampleWithError() {
         try {
             val gson = GsonBuilder().create()
             // 存在しないファイルを開く
@@ -90,13 +115,33 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            if(sample != null){
+            if (sample != null) {
                 txt_name.setText(sample.name)
                 txt_age.setText(sample.age.toString())
             }
-        }catch (ex:Exception){
+        } catch (ex: Exception) {
             Timber.e(ex)
         }
     }
 
+    private fun saveDatabase() {
+        coroutineScope.launch {
+            withContext(Dispatchers.IO) {
+                // 保存するデータを準備(uidは、仮の値。保存時に上書きされる)
+                val user = User(0, txt_name.text.toString(), txt_age.text.toString().toInt())
+                db.userDao().insert(user)
+            }
+        }
+    }
+
+    private fun loadDatabase() {
+        coroutineScope.launch {
+            withContext(Dispatchers.IO) {
+                val users = db.userDao().getAll()
+                users.forEach {
+                    Timber.d("user = ${it.uid}/ ${it.name}/ ${it.age}")
+                }
+            }
+        }
+    }
 }
