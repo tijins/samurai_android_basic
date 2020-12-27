33a64c088d99277374c74dd0c287292d7826c238
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2020-12-27 11:20:05 +0900

    Contentを表示できるようにする

--- app/src/main/java/com/esp/basicapp/SecondActivity.kt
+++ app/src/main/java/com/esp/basicapp/SecondActivity.kt
@@ -2,10 +2,17 @@ package com.esp.basicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+    companion object{
+        const val EXTRA_CONTENT = "extra_content"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        content.text = intent.getStringExtra(EXTRA_CONTENT) ?: "EXTRA_CONTENTが指定されていません"
     }
 }
