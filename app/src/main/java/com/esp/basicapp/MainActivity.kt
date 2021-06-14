fe07cec8c82b9eb287a6cabe83d679be0daf7f2e
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-06-14 10:27:55 +0900

    layoutを追加

--- app/src/main/java/com/esp/basicapp/MainActivity.kt
+++ app/src/main/java/com/esp/basicapp/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.esp.basicapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.esp.basicapp.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         Timber.d("onCreate")
     }
